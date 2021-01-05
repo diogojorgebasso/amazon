abb01bd58dd901024dad5437bba601d69c9476af
Diogo Basso <diogojbasso@gmail.com>
2021-01-05 00:19:28 -0300

    initial files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6v8xFomwqUATIB4pafLebMCUA78uAS90",
+  authDomain: "ecommerce-7abf9.firebaseapp.com",
+  projectId: "ecommerce-7abf9",
+  storageBucket: "ecommerce-7abf9.appspot.com",
+  messagingSenderId: "678195067320",
+  appId: "1:678195067320:web:0d2fe43fde66a10f38374d",
+  measurementId: "G-0JVVVVQ1ZX"
+};
