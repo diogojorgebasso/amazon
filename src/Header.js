ac0825cf3727b5f6004e1e37077aadb7e44f4bc4
Diogo Basso <diogojbasso@gmail.com>
2021-01-06 17:46:11 -0300

    start header

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,25 @@ import { View } from 'react-native';
 
 export default function src() {
  return (
-   <View/>
+    <div className="header">
+      <img className="headerLogo" src="../public/amazonlogo.png" alt="Amazon Logo" />
+      <div className="headerSearch">
+        <input type="text" className="headerSearchInput"/>
+      
+      </div>
+      <div className="headerNav">
+        <div className="headerOption">
+          <span className="HeaderHello">Hello Guest</span>
+          <span className="HeaderHello">Sign In</span>
+        </div>
+        <div className="headerOption">
+          <span className="HeaderReturns">Return</span>
+          <span className="HeaderHello">& orders</span>
+        </div>
+        <div className="headerOption"></div>
+          <span className="HeaderHello">Your</span>
+          <span className="HeaderHello">Prime</span>
+      </div>
+    </div>
   );
 }
