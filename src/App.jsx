7f7e4415ffade8461ae9aa83ff91772796c5570a
Diogo Basso <diogojbasso@gmail.com>
2021-01-30 15:43:50 -0300

    feat: implementing verification of user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import Header from "./Header";
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./Home";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
-
+import { auth } from "./firebase";
 function App() {
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+      if (authUser) {
+      } else {
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,5 +1,6 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 //selector
 export const GetBasketTotal = (basket) => {
