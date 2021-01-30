7760d46657e64f7ea43be4e54572abaa2bd5abe0
Diogo Basso <diogojbasso@gmail.com>
2021-01-30 15:17:04 -0300

    fix: firebase API

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,17 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA6v8xFomwqUATIB4pafLebMCUA78uAS90",
-  authDomain: "ecommerce-7abf9.firebaseapp.com",
-  projectId: "ecommerce-7abf9",
-  storageBucket: "ecommerce-7abf9.appspot.com",
-  messagingSenderId: "678195067320",
-  appId: "1:678195067320:web:0d2fe43fde66a10f38374d",
-  measurementId: "G-0JVVVVQ1ZX",
-};
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
+
+const db = firebaseApp.firestore();
+const auth;
