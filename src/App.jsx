5bb4f4b2f1199c985328ffa0665b69b041c072ab
Diogo Basso <diogojbasso@gmail.com>
2021-01-30 20:46:31 -0300

    feat: reducer

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,23 @@ import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       console.log(authUser);
       if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
       }
     });
   }, []);
